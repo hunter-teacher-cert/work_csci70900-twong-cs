95e1c2e108dfcf2eaa249e0195d138056a44bed7
Tiffany Wong <wongt1227@gmail.com>
2021-07-13 11:28:41 -0400

    printed board for knights

--- ds/maze/Driver.java
+++ ds/maze/Driver.java
@@ -5,15 +5,17 @@ public class Driver{
 
     public static void main(String[] args) {
 	  boolean solved;
-
+/*
 	  Maze m = new Maze("maze.dat");
 	  solved = m.solve(1,1);
+*/
 
-	/*
-	  Knights k - new Knights(5);
-	  solved = k.solve(1,1);
+	  Knights k = new Knights(5);
+    System.out.println(k);
+	  solved = k.solve(1,1,0);
 
-	*/
+
+
   //Knights k = new Knights(5);
   //k.solve(2,2,1);
     }
