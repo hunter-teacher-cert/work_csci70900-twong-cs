ab7d1f4246b128e169fce87e20ace2f52f3e2b9b
Tiffany Wong <wongt1227@gmail.com>
2021-07-20 13:35:27 -0400

    merge in driver

--- ds/sort1/SortDemoDriver.java
+++ ds/sort1/SortDemoDriver.java
@@ -6,15 +6,16 @@ import java.util.*;
 
 public class SortDemoDriver {
   public static void main(String[] args) {
+
   //default constructor limited to 15
 	SortDemo sd = new SortDemo();
-	System.out.println(sd);
+	//System.out.println(sd);
   //value constructor with the int
 	SortDemo sd2 = new SortDemo(30);
-	System.out.println(sd2);
+	//System.out.println(sd2);
 
 	/* part 2 test lines */
-
+  /*
 	int i;
 	i = sd.findSmallestIndex(0);
   //System.out.println(i);  //check to see if the program returns the index of the smallest number
@@ -22,26 +23,26 @@ public class SortDemoDriver {
 	i = sd2.findSmallestIndex(0);
   //System.out.println(i);
 	System.out.println("sd2["+ i + "] = "+sd2.get(i)+" : " + sd2);
-
+  */
 
 	/* part 3 test lines */
 
 	//System.out.println(sd);
+  /*
 	sd.sort();
   System.out.print("Sorted list sd: ");
 	System.out.println(sd);
-  System.out.println(sd.binarySearch(12));
-  /*
+  //System.out.println(sd.binarySearch(12));
+
   sd2.sort();
   System.out.print("Sorted list sd2: ");
 	System.out.println(sd2);
-  System.out.println(sd2.binarySearch(12));
-  */
+*/
+  //System.out.println(sd2.binarySearch(12));
+
   //System.out.println(sd.linearSearch(0));
 
-
-
-
+  sd.testMerge();
 
   }
 }
