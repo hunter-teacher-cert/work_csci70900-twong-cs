1396b241dae6fd4edbb11b170a3749ff790b53ba
Tiffany Wong <wongt1227@gmail.com>
2021-07-29 11:55:57 -0400

    knights tour driver

---
+++ ds/knights/Driver.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args) {
+	  boolean solved;
+
+	  Knights k = new Knights(3);
+	  solved = k.solve(2,2,0);
+
+  //Knights k = new Knights(5);
+  //k.solve(2,2,1);
+    }
+}
